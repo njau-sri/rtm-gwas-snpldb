7d6dd54168af01796975af7a29f09c51817dfa15
Jianbo He <hjbxyz@gmail.com>
2018-11-04 14:04:17 +0800

    Update install

--- install-macos.sh
+++ install-macos.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+LIB=/usr/local/lib
+
+LIBGCC=${LIB}/gcc/x86_64-apple-darwin17.5.0/8.1.0/libgcc.a
+
 rm -rf macos
 mkdir macos
 
-LIB=/usr/local/lib
-LIBGCC=${LIB}/gcc/x86_64-apple-darwin17.5.0/8.1.0/libgcc.a
-
-g++ *.cpp -o macos/rtm-gwas-snpldb -O2 -std=c++11 $LIBGCC
+g++ *.cpp -o macos/rtm-gwas-snpldb -O2 -std=c++11 ${LIBGCC}
