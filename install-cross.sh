388e6ebbb6f3169640fa3c9616fb15011406bcf9
Jianbo He <hjbxyz@gmail.com>
2019-04-20 12:57:27 +0800

    Move source to src

--- install-cross.sh
+++ install-cross.sh
@@ -11,17 +11,17 @@ fi
 
 if [ $1 == "glnx64" ]; then
 
-    g++ *.cpp -o $TARGET -s -O2 -std=c++11 -static -fopenmp \
+    g++ src/*.cpp -o $TARGET -s -O2 -std=c++11 -static -fopenmp \
         -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
 
 elif [ $1 == "win32" ]; then
 
-    i686-w64-mingw32-g++ *.cpp -o $TARGET.exe -s -O2 -std=c++11 -static -fopenmp \
+    i686-w64-mingw32-g++ src/*.cpp -o $TARGET.exe -s -O2 -std=c++11 -static -fopenmp \
         -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
 
 elif [ $1 == "win64" ]; then
 
-    x86_64-w64-mingw32-g++ *.cpp -o $TARGET.exe -s -O2 -std=c++11 -static -fopenmp \
+    x86_64-w64-mingw32-g++ src/*.cpp -o $TARGET.exe -s -O2 -std=c++11 -static -fopenmp \
         -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
 
 fi
--- install-macos.sh
+++ install-macos.sh
@@ -9,5 +9,5 @@ if [ -z "$RTM_GWAS_VERSION" ]; then
     RTM_GWAS_VERSION=unknown
 fi
 
-g++ *.cpp -o $TARGET -O2 -std=c++11 \
+g++ src/*.cpp -o $TARGET -O2 -std=c++11 \
     -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
