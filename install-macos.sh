c117045e583000c1b392e6670849edc3eead7edf
Jianbo He <hjbxyz@gmail.com>
2019-04-30 14:09:12 +0800

    Use clang on macOS

--- install-macos.sh
+++ install-macos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export PATH="/usr/local/opt/llvm@7/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/llvm@7/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm@7/include"
+
 rm -rf macos
 mkdir macos
 
@@ -9,5 +13,5 @@ if [ -z "$RTM_GWAS_VERSION" ]; then
     RTM_GWAS_VERSION=unknown
 fi
 
-g++ src/*.cpp -o $TARGET -O2 -std=c++11 \
+clang++ src/*.cpp -o $TARGET -O2 -std=c++11 -fopenmp \
     -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
